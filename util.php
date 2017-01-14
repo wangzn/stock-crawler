974ef1c6c32dfb1aed5032ced537a9068ef3e204
wangzn <wangzn04@gmail.com>
2017-01-14 23:12:40 +0800

    add http_request to utils

--- util.php
+++ util.php
@@ -5,3 +5,19 @@ function plist($l)
         echo "$k: $v\n";
     }
 }
+
+function http_request($url, $data = false)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+    if ($data !== false) {
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+    $body = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    $info["body"] = $body;
+    return $info;
+}
